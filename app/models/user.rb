9c0952649a9cb7d4497fb1cd53402267f43c4a53
sareid <andrew.reid@flatironschool.com>
2016-01-27 22:29:11 -0500

    Adds user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :skills, foreign_key: 'teacher_id'
-
+  has_many :tutorials, foreign_key: 'student_id', class_name: "Connection"
+  has_many :connections, foreign_key: 'teacher_id'
+  has_many :teachers, through: :tutorials
+  has_many :students, through: :connections
 end
