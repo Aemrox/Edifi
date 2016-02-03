6fe87c3123e16ca85477fc94b0a6ff8a755ba849
ruchiramani <ruchiramani@gmail.com>
2016-02-03 12:11:28 -0500

    Adds partial functionality for inbox message tab

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,14 +3,13 @@ class ConversationsController < ApplicationController
   before_filter :authenticate!
 
   def create
-    binding.pry
+    
     if Conversation.between(params[:sender_id],params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
     end
-
-    redirect_to conversation(@conversation)
+    redirect_to @conversation
 
   end
 
