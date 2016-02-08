622817ddf17706c52ffe1920c7c11d1f13e5ad9f
Aemrox <adam.may2164@gmail.com>
2016-02-08 09:47:14 -0500

    Tested login functionality, it all works

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160205194600) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,8 +98,8 @@ ActiveRecord::Schema.define(version: 20160205194600) do
     t.datetime "updated_at",      null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "attachment"
     t.text     "availability"
+    t.string   "attachment"
     t.string   "provider"
     t.string   "uid"
   end
